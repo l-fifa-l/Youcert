568507499645abd0739a5990de72d5b684305ba4
Vivek <starktestic@gmail.com>
2022-03-15 13:43:44 +0530

    Update server.js

--- client/server.js
+++ client/server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const next = require("next");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const express = require('express');
+const next = require('next');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -13,23 +13,23 @@ app
     //apply proxy in dev mode
     if (dev) {
       server.use(
-        "/api",
+        '/api',
         createProxyMiddleware({
-          target: "http://localhost:8000",
+          target: 'http://localhost:8000',
           changeOrigin: true,
         })
       );
     }
 
-    server.all("*", (req, res) => {
+    server.all('*', (req, res) => {
       return handle(req, res);
     });
 
     server.listen(3000, (err) => {
       if (err) throw err;
-      console.log("> Ready on http://localhost:8000");
+      console.log('> Ready on http://localhost:8000');
     });
   })
   .catch((err) => {
-    console.log("Error,err");
+    console.log('Error,err');
   });
